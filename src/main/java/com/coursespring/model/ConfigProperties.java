2899703afbb764f11a99ce504be8ceed6906bc61
JeffersonLuizCruz <jefferson.luiz.cruz@gmail.com>
2023-08-16 23:00:03 -0300

    Revert "refat"
    This reverts commit f3cfd296c53766e9a7fbbad4c321ccd190f5bc0a.

--- src/main/java/com/coursespring/model/ConfigProperties.java
+++ src/main/java/com/coursespring/model/ConfigProperties.java
@@ -1,11 +1,11 @@
 package com.coursespring.model;
 
-import java.io.Serializable;
-
+import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.stereotype.Component;
 
-import lombok.Data;
+import java.io.Serializable;
 
 @ConfigurationProperties
 @Component
